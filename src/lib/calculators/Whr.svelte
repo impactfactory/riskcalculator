ed71e7c1b023263e72baf39fa89b25f73b576125
Kai <webmaster@impactfactory.ch>
2022-10-06 11:38:23 +0200

    improvement whr calculator

--- src/lib/calculators/Whr.svelte
+++ src/lib/calculators/Whr.svelte
@@ -5,25 +5,31 @@
 </script>
 
 {#if isFinite(parseFloat((taille / hip).toFixed(3)))}
+	{@const whr = taille / hip}
 	<h3 class="pb-0 mb-0">
-		WHR: {parseFloat((taille / hip).toFixed(2))} /
+		WHR: {whr.toFixed(2)} /
 		{#if gender == 'weiblich'}
-			{#if parseFloat((taille / hip).toFixed(3)) <= 0.8}
+			{#if whr <= 0.8}
 				<span class="normal">Normalgewicht</span>
-			{:else if parseFloat((taille / hip).toFixed(3)) > 0.8 && parseFloat((taille / hip).toFixed(3)) <= 0.84}
+			{:else if whr > 0.8 && whr <= 0.849}
 				<span class="over">Übergewicht</span>
-			{:else if parseFloat((taille / hip).toFixed(3)) > 0.84}
+			{:else if whr > 0.849}
 				<span class="adi">Adipositas</span>
 			{/if}
 		{:else if gender == 'männlich'}
-			{#if parseFloat((taille / hip).toFixed(3)) <= 0.9}
+			{#if whr <= 0.9}
 				<span class="normal">Normalgewicht</span>
-			{:else if parseFloat((taille / hip).toFixed(3)) > 0.9 && parseFloat((taille / hip).toFixed(3)) <= 0.99}
+			{:else if whr > 0.9 && whr <= 0.99}
 				<span class="over">Übergewicht</span>
-			{:else if parseFloat((taille / hip).toFixed(3)) > 0.99}
+			{:else if whr > 0.99}
 				<span class="adi">Adipositas</span>
 			{/if}
 		{/if}
 	</h3>
-	<small>* Waste to Hip Ratio / Taille-Umfang durch Hüftumfang</small>
+	<small
+		>* Waste to Hip Ratio / Taille-Umfang durch Hüftumfang. <a
+			href="http://apps.who.int/iris/bitstream/handle/10665/44583/9789241501491_eng.pdf;jsessionid=71DEE026302DBDCA957DC0D970598D8B?sequence=1"
+			rel="noopener">Quelle Berechnungsmethodik</a
+		></small
+	>
 {/if}
